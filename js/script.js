6c839f06da50fe123dc7f34e3c15b446cca92f2d
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-28 10:45:44 +0200

    Create a createParaghraph function. And change script tag from defer to "module" type.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 // Dom elements
 let pokemonContainer = document.getElementById("pokemon-container")
-console.log(pokemonContainer)
 
-pokemonContainer.textContent = "It works"
+
+// creates a clickable paragraph tag, with given text
+function createParagraph(text) {
+    // create the element in a variable and set its text
+    let paragraphElement = document.createElement("p")
+    paragraphElement.textContent = text
+    // add click event to the paragraphElement (not finished!)
+    paragraphElement.addEventListener("click", console.log)
+
+    // attach the element to the container
+    pokemonContainer.append(paragraphElement)
+}
+
+// run the function (to see if it works)
+createParagraph("Test")
+
 
 // API - Application Programming Interface
 

ea46b4e66a7be27fedaf912ccc1e68e8a9dbaab0
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-28 10:21:25 +0200

    setup async function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-// API 
+// API - Application Programming Interface
+
 // endpoint: https://pokeapi.co/api/v2/pokemon
 
-let mainUrl = "https://pokeapi.co/api/v2/pokemon"
 
 
 
 
-async function fetchData() {
+async function getData() {
+    
+    // 1. make a request and store the response
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon")
+    // console.log(response)
 
-    let pokemonData = await fetch(mainUrl)
+    // 2. get data from the response
+    let data = await response.json()
+    // console.log(data)
 
-    console.log(pokemonData)
+    // now that we have data, we can do something with it :)
+    // lets try show the results...
+ 
+    for (let pokemon of data.results) {
+        console.log(pokemon)
+    }
 
 }
 
-fetchData() 
+
+let newData = getData() // invoke (kjøre) funksjonen
+
+
+
